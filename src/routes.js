59fff250246219b8f050a3c5bc04861b5751bf84
darshanwohlig19 <darshan.gada@wohlig.com>
2025-10-13 16:09:28 +0530

    updates

--- src/routes.js
+++ src/routes.js
@@ -6,11 +6,14 @@ import { FiTrash2 } from 'react-icons/fi'
 import { useParams } from 'react-router-dom'
 import { FaYoutube } from 'react-icons/fa'
 import { ChartNoAxesCombined } from 'lucide-react'
-import Dashboard from '../src/assets/svg/dashboard.svg'
+import DashboardIcon from '../src/assets/svg/dashboard.svg'
 import { FaFolderOpen } from 'react-icons/fa'
 import { FaUsers } from 'react-icons/fa'
+import Dashboard from 'views/dashboard'
 import Organizations from 'views/organizations'
 import OrganizationDetail from 'views/organizations/OrganizationDetail'
+import { getEncryptedCookie } from 'common/utils/cookieUtils'
+
 const RoutesComponent = () => {
   const { id } = useParams()
   const [projectName, setProjectName] = useState()
@@ -23,12 +26,18 @@ const RoutesComponent = () => {
   }, [localStorage.getItem('projectName')])
 
   useEffect(() => {
+    // Get user role from encrypted cookie
+    const userRole = getEncryptedCookie('userRole')
+    const isSuperAdmin = userRole === 'superadmin'
+
     setRoutes([
       {
         name: 'Sign In',
         path: 'sign-in',
         layout: '/afterLogin',
-        icon: <img src={Dashboard} alt="Dashboard Icon" className="h-6 w-6" />,
+        icon: (
+          <img src={DashboardIcon} alt="Dashboard Icon" className="h-6 w-6" />
+        ),
         component: <SignIn />,
         isComing: false,
         isVisible: false,
@@ -37,7 +46,9 @@ const RoutesComponent = () => {
         name: 'Sign Up',
         path: 'sign-up',
         layout: '/afterLogin',
-        icon: <img src={Dashboard} alt="Dashboard Icon" className="h-6 w-6" />,
+        icon: (
+          <img src={DashboardIcon} alt="Dashboard Icon" className="h-6 w-6" />
+        ),
         component: <SignUp />,
         isComing: false,
         isVisible: false,
@@ -49,7 +60,18 @@ const RoutesComponent = () => {
         icon: <FaUsers className="h-6 w-6" />,
         component: <Organizations />,
         isComing: false,
-        isVisible: true,
+        isVisible: isSuperAdmin, // Only visible to superadmin
+      },
+      {
+        name: 'Dashboard',
+        path: 'dashboard',
+        layout: '/afterLogin',
+        icon: (
+          <img src={DashboardIcon} alt="Dashboard Icon" className="h-6 w-6" />
+        ),
+        component: <Dashboard />,
+        isComing: false,
+        isVisible: isSuperAdmin, // Only visible to superadmin
       },
       {
         name: 'Organization Details',
