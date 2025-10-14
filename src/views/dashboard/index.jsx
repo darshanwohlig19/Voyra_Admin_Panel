81427b0995198688cfbd3a1ad458414081ccddac
darshanwohlig19 <darshan.gada@wohlig.com>
2025-10-14 17:15:39 +0530

    updates

--- src/views/dashboard/index.jsx
+++ src/views/dashboard/index.jsx
@@ -1162,7 +1162,7 @@ const Dashboard = () => {
                 className="rounded-lg border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 outline-none transition duration-200 hover:border-gray-400 focus:border-brand-500 focus:ring-2 focus:ring-brand-500 focus:ring-opacity-50 dark:border-gray-600 dark:bg-navy-700 dark:text-white dark:hover:border-gray-500"
               >
                 <option value="day">1 Day</option>
-                <option value="week">7 Days</option>
+                <option value="week">Week</option>
                 <option value="month">Month</option>
                 <option value="year">Year</option>
               </select>
@@ -1202,7 +1202,7 @@ const Dashboard = () => {
                 className="rounded-lg border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 outline-none transition duration-200 hover:border-gray-400 focus:border-brand-500 focus:ring-2 focus:ring-brand-500 focus:ring-opacity-50 dark:border-gray-600 dark:bg-navy-700 dark:text-white dark:hover:border-gray-500"
               >
                 <option value="day">1 Day</option>
-                <option value="week">7 Days</option>
+                <option value="week">Week</option>
                 <option value="month">Month</option>
                 <option value="year">Year</option>
               </select>
@@ -1239,7 +1239,7 @@ const Dashboard = () => {
                 className="rounded-lg border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 outline-none transition duration-200 hover:border-gray-400 focus:border-brand-500 focus:ring-2 focus:ring-brand-500 focus:ring-opacity-50 dark:border-gray-600 dark:bg-navy-700 dark:text-white dark:hover:border-gray-500"
               >
                 <option value="day">1 Day</option>
-                <option value="week">7 Days</option>
+                <option value="week">Week</option>
                 <option value="month">Month</option>
                 <option value="year">Year</option>
               </select>
